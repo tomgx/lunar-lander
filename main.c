c4956fcd81bff855bc447e15e3d723132b264bbd
Thomas <t.green01q@gmail.com>
2022-04-28 02:06:55 +0100

    basic main menu, can detect one arrow key press

--- main.c
+++ main.c
@@ -1,10 +1,46 @@
-#include <ncurses.h>
+#include "main.h"
+
 
 int main() {
+    
     initscr(); //create the screen
-    printw("Hello World!"); //print hello world
-    refresh(); //print on to real screen
-    getch(); //wait for user input
+    raw();				/* Line buffering disabled	*/
+    attron(A_UNDERLINE);
+    printw("Welcome to Lunar Lander\n"); //print hello world
+    attroff(A_UNDERLINE);
+    attron(A_STANDOUT);
+    printw("PLAY\n");
+    attroff(A_STANDOUT);
+    printw("EXIT\n");
+    noecho();			/* Don't echo() while we do getch */
+
+    raw();				/* Line buffering disabled	*/
+	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
+	noecho();			/* Don't echo() while we do getch */
+	ch = getch();			/* If raw() hadn't been called
+					 * we have to press enter before it
+					 * gets to the program 		*/
+
+
+
+     switch (ch) {
+          case KEY_UP: /* user pressed backspace */ 
+          printw("UP arrow is pressed\n");
+          break;
+          case KEY_DOWN:  /* user pressed up arrow key */
+          printw("DOWN arrow is pressed\n");
+          break;
+          case KEY_LEFT:  /* user pressed up arrow key */
+          printw("LEFT arrow is pressed\n");
+          break;
+          case KEY_RIGHT:   /* user pressed key 'A' */
+          printw("RIGHT arrow is pressed\n");
+          break;
+     }
+
+    refresh();			/* Print it on to the real screen */
+
+    getch(); // wait for user input before exiting
     endwin(); //end curses mode
 
     return 0;
